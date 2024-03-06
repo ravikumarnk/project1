2ee42e8f34a63c747f8b4bc050d5ca3b09bca373
ravikumar <ravikelakam@gmail.com>
2024-03-06 10:51:55 +0530

    first commit

---
+++ src/main/java/project001/projectex/App.java
@@ -0,0 +1,13 @@
+package project001.projectex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven Project demo");
+}
+}
---
+++ src/test/java/project001/projectex/AppTest.java
@@ -0,0 +1,23 @@
+package project001.projectex;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("testing application... ");
+    	assertTrue( true );
+        
+        
+    }
+}
